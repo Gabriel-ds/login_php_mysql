fd85eccec478038f42e151f00b848d5c9b859544
Gabriel dos Santos <gabrields1910@gmail.com>
2021-08-29 18:29:48 -0300

    Implementação visual do projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="corpo-form">
+        <form action="processa.php" method="post">
+            <h1>ENTRAR</h1>
+            <input type="e-mail" placeholder="Usuário">
+            <input type="password" placeholder="Senha">
+            <input type="submit" value="ACESSAR">
+            <a href="">Ainda não é inscrito? <strong>Cadastre-se</strong></a>
+        </form>
+    </div>
+
+</body>
+
+</html>
